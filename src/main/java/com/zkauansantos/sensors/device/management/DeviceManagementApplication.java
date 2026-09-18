0da7a1ddb64079fd98d781c6c8aeb9773da060e2
Kauan Santos <kauansantosdepontes@gmail.com>
2026-09-18 15:17:16 -0300

    chore: init all submodules

---
+++ src/main/java/com/zkauansantos/sensors/device/management/DeviceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zkauansantos.sensors.device.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zkauansantos/sensors/device/management/DeviceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkauansantos.sensors.device.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
